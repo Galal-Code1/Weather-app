4a46d1fe223a7ffd747d517b26a0d829242708a7
galal <almaryjlal85@gmail.com>
2026-09-21 20:09:29 +0300

    Complete design for desktop

---
+++ app.js
@@ -0,0 +1,16 @@
+// const latitude = 15.3694;
+// const longitude = 44.1910;
+
+// const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,wind_speed_10m`;
+
+// fetch(url)
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+
+//         console.log("Temperature:", data.current.temperature_2m);
+//         console.log("Wind:", data.current.wind_speed_10m);
+//     })
+//     .catch(error => {
+//         console.error("Error:", error);
+//     });
